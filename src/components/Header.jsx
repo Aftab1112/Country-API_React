7eb4820df0a80b56c927e7afc899804608fe4fe9
Aftab1112 <you@example.com>
2024-02-11 10:03:07 +0530

    added dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Header() {
+  const [isDark, setIsDark] = useState(
+    JSON.parse(localStorage.getItem("isDarkMode"))
+  );
+
+  if (isDark) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+
   return (
     <header className="header-container">
       <div className="header-content">
-        <h2 className="tittle"><a href="/">Where In The World ?</a></h2>
-        <p className="theme-changer">
-          <i className="fa-regular fa-moon"></i>&nbsp;&nbsp;Dark Mode
+        <h2 className="tittle">
+          <a href="/">Where In The World ?</a>
+        </h2>
+        <p
+          className="theme-changer"
+          onClick={() => {
+            setIsDark(!isDark);
+            localStorage.setItem("isDarkMode", !isDark);
+          }}
+        >
+          <i className={`fa-solid fa-${isDark ? "sun" : "moon"}`}></i>
+          &nbsp;&nbsp;{`${isDark ? "Light Mode" : "Dark Mode"}`}
         </p>
       </div>
     </header>
-  )
+  );
 }
